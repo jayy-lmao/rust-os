705d076c6b4a37bd3ffa4e9a044aa160d9602952
James Holman <james.holman@domain.com.au>
2020-02-08 19:55:50 +1100

    :alembic: page 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 #![no_std]
+#![no_main] // disable rust entry point
 
 use core::panic::PanicInfo;
 
+#[no_mangle] // dont mangle names
+pub extern "C" fn _start() -> ! {
+  loop {}
+}
+
+
+// Panic func
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
   loop {}
 }
-
-fn main() {}
