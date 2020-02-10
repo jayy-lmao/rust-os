521f266150e1f3b7880e3947894bcbb5ee35c06a
James Holman <james.holman@domain.com.au>
2020-02-10 18:25:58 +1100

    :alembic: newlines now work

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 #![no_std]
 #![no_main] // disable rust entry point
+#![feature(exclusive_range_pattern)]
+
+
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 
 static HELLO: &[u8] = b"Yetus my feetus!";
 #[no_mangle] // dont mangle names
 pub extern "C" fn _start() -> ! {
-  let vga_buffer = 0xb8000 as *mut u8;
-
-  for (i, &byte) in HELLO.iter().enumerate() {
-    unsafe {
-      *vga_buffer.offset(i as isize * 2) = byte;
-      *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    }
-  }
+  vga_buffer::print_something();
 
   loop{}
 }
